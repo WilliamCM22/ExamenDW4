078f10e05090d303aa4fd9722a3ae67a2f08858e
William Castro <william.castromiller@outlook.com>
2023-03-13 09:57:12 -0400

    1

---
+++ ExamenDW4/Controllers/AppartementController.cs
@@ -0,0 +1,61 @@
+﻿using ExamenDW4.Data;
+using ExamenDW4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamenDW4.Controllers
+{
+    public class AppartementController : Controller
+    {
+        private readonly Bails _bails;
+
+        public AppartementController(Bails bails)
+        {
+            _bails= bails;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var appartement = await _bails.Appartements.ToListAsync();
+            return View(appartement);
+        }
+
+        public async Task<IActionResult> Modification(int id)
+        {
+            var appartement = await _bails.Appartements.FindAsync(id);
+
+            if (appartement != null)
+            {
+                return View(appartement);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Modification(int id, Appartement donnees)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(donnees);
+            }
+
+            var appartement = await _bails.Appartements.FindAsync(id);
+
+            if (appartement != null)
+            {
+                
+                appartement.Numero = donnees.Numero;
+                appartement.Surface = donnees.Surface;
+                appartement.Meuble = donnees.Meuble;
+                appartement.TypeAppartement = donnees.TypeAppartement;
+
+                await _bails.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return NotFound();
+        }
+    }
+}
--- ExamenDW4/Data/Donnees.cs
+++ ExamenDW4/Data/Donnees.cs
@@ -9,67 +9,88 @@ namespace ExamenDW4.Data
         {
             await bails.Database.MigrateAsync();
 
-            //if (await bails.Appartements.AnyAsync())
-            //{
-            //    return;
-            //}
+            if (await bails.Appartements.AnyAsync())
+            {
+                return;
+            }
 
-            //var app20 = new Appartement()
-            //{
-            //    Numero = 20,
-            //    Surface = 600,
-            //    Meuble = true,
-            //    Type = Models.Type.Loft
-            //};
+            var app20 = new Appartement()
+            {
+                Numero = 20,
+                Surface = 600,
+                Meuble = true,
+                TypeAppartement = TypeAppartement.Loft
+            };
 
-            //var app30 = new Appartement()
-            //{
-            //    Numero = 30,
-            //    Surface = 700,
-            //    Meuble = true,
-            //    Type = Models.Type.Studio
-            //};
+            var app30 = new Appartement()
+            {
+                Numero = 30,
+                Surface = 700,
+                Meuble = true,
+                TypeAppartement = TypeAppartement.Studio
+            };
 
-            //var app40 = new Appartement()
-            //{
-            //    Numero = 40,
-            //    Surface = 800,
-            //    Meuble = false,
-            //    Type = Models.Type.Studio
-            //};
+            var app40 = new Appartement()
+            {
+                Numero = 40,
+                Surface = 800,
+                Meuble = false,
+                TypeAppartement = TypeAppartement.Studio
+            };
 
-            //var app50 = new Appartement()
-            //{
-            //    Numero = 50,
-            //    Surface = 750,
-            //    Meuble = false,
-            //    Type = Models.Type.Penthouse
-            //};
+            var app50 = new Appartement()
+            {
+                Numero = 50,
+                Surface = 750,
+                Meuble = false,
+                TypeAppartement = TypeAppartement.Penthouse
+            };
 
 
-            //var alexandre = new Locataire()
-            //{
-            //    Nom = "Robitaille",
-            //    Prenom = "Alexandre"
-            //};
+            var alexandre = new Locataire()
+            {
+                Nom = "Robitaille",
+                Prenom = "Alexandre"
+            };
 
-            //var mariesoleil = new Locataire()
-            //{
-            //    Nom = "Dionne",
-            //    Prenom = "Marie-Soleil"
-            //};
+            var mariesoleil = new Locataire()
+            {
+                Nom = "Dionne",
+                Prenom = "Marie-Soleil"
+            };
 
-            //var deborah = new Locataire()
-            //{
-            //    Nom = "Guenova",
-            //    Prenom = "Deborah"
-            //};
+            var deborah = new Locataire()
+            {
+                Nom = "Guenova",
+                Prenom = "Deborah"
+            };
 
-            //var location1 = new Location() 
-            //{
-                
-            //}
+            var location1 = new Location()
+            {
+                Appartement = app30,
+                Locataire = alexandre,
+                LoyerMensuel = 1200,
+                DateDebut = new DateTime(2023, 01, 01),
+                DateFin = new DateTime(2024, 06, 30)
+            };
 
+            var location2 = new Location()
+            {
+                Appartement = app40,
+                Locataire = deborah,
+                LoyerMensuel = 1600,
+                DateDebut = new DateTime(2022, 04, 01),
+                DateFin = new DateTime(2024, 06, 30)
+            };
+
+            var location3 = new Location()
+            {
+                Appartement = app50,
+                Locataire = mariesoleil,
+                LoyerMensuel = 1100,
+                DateDebut = new DateTime(2021, 11, 01),
+                DateFin = new DateTime(2022, 10, 30)
+            };
         }
     }
 }
